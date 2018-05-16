8cf1fb0f419265595b29e44c78a88865acadb53a
therealloker <therealloker@gmail.com>
2018-05-16 21:40:57 +0300

    refactored method 'new'

--- factory.rb
+++ factory.rb
@@ -1,4 +1,6 @@
-module Factory
+class Factory
+  include Enumerable
+
   def self.new(*args, &block)
     if name_is_valid?(args.first)
       name = args.shift
@@ -28,6 +30,12 @@ module Factory
     Class.new(Factory) do
       attr_accessor(*args)
 
+      define_singleton_method :new do |*attrs|
+        inst = allocate
+        inst.send(:initialize, *attrs)
+        inst
+      end
+
       define_method :initialize do |*attrs|
         args.zip(attrs) do |method, value|
           send("#{method}=", value)
@@ -37,72 +45,68 @@ module Factory
     end
   end
 
-  class Factory
-    include Enumerable
+  def ==(other)
+    self.class == other.class && values == other.values
+  end
 
-    def ==(other)
-      self.class == other.class && values == other.values
+  alias eql? ==
+
+  def [](key)
+    if key.is_a?(Integer)
+      msg = "offset #{key} too large for factory(size:#{length})"
+      raise IndexError, msg if key > length - 1
+      instance_variable_get(instance_variables[key])
+    else
+      send(key.to_sym)
     end
+  end
 
-    alias eql? ==
-
-    def [](key)
-      if key.is_a?(Integer)
-        msg = "offset #{key} too large for factory(size:#{length})"
-        raise IndexError, msg if key > length - 1
-        instance_variable_get(instance_variables[key])
-      else
-        send(key.to_sym)
-      end
+  def []=(key, value)
+    if key.is_a?(Integer)
+      iv = instance_variables[key]
+      instance_variable_set(iv, value)
+    else
+      send("#{key.to_sym}=", value)
     end
+  end
 
-    def []=(key, value)
-      if key.is_a?(Integer)
-        iv = instance_variables[key]
-        instance_variable_set(iv, value)
-      else
-        send("#{key.to_sym}=", value)
-      end
-    end
+  def to_a
+    instance_variables.inject([]) { |arr, iv| arr << instance_variable_get(iv) }
+  end
 
-    def to_a
-      instance_variables.inject([]) { |arr, iv| arr << instance_variable_get(iv) }
-    end
+  alias values to_a
 
-    alias values to_a
+  def values_at(*selector)
+    values.values_at(*selector)
+  end
 
-    def values_at(*selector)
-      values.values_at(*selector)
-    end
+  def members
+    instance_variables.inject([]) { |mem, iv| mem << iv.to_s.sub('@', '').to_sym }
+  end
 
-    def members
-      instance_variables.inject([]) { |mem, iv| mem << iv.to_s.sub('@', '').to_sym }
-    end
+  def length
+    members.length
+  end
 
-    def length
-      members.length
-    end
+  alias size length
 
-    alias size length
+  def to_h
+    members.zip(values).to_h
+  end
 
-    def to_h
-      members.zip(values).to_h
-    end
+  def each(&block)
+    block_given? ? values.send(:each, &block) : enum_for(:each)
+  end
 
-    def each(&block)
-      block_given? ? values.send(:each, &block) : enum_for(:each)
-    end
+  def each_pair(&block)
+    block_given? ? to_h.send(:each, &block) : enum_for(:each)
+  end
 
-    def each_pair(&block)
-      block_given? ? to_h.send(:each, &block) : enum_for(:each)
-    end
+  def dig(*args)
+    to_h.send(:dig, *args)
+  end
 
-    def dig(*args)
-      to_h.send(:dig, *args)
-    end
-
-    def select(&block)
-      block_given? ? values.send(:select, &block) : enum_for(:select)
-    end
+  def select(&block)
+    block_given? ? values.send(:select, &block) : enum_for(:select)
   end
 end
